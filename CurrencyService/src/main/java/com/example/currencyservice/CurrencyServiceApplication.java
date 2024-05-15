240ffb0da0984ac1072fc31f56613554f5987fbb
ipiven <ipiven@uma.tech>
2024-05-15 15:33:57 +0300

    Enable caching

--- CurrencyService/src/main/java/com/example/currencyservice/CurrencyServiceApplication.java
+++ CurrencyService/src/main/java/com/example/currencyservice/CurrencyServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.currencyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableCaching
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class CurrencyServiceApplication {
 
